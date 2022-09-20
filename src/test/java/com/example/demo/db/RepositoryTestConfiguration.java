267287754c2a209c2c46264b75c8583d1aa010e3
Lars Kaulen <lars.kaulen@outlook.de>
2022-09-20 10:49:18 +0200

    Add tests and required configuration

---
+++ src/test/java/com/example/demo/db/RepositoryTestConfiguration.java
@@ -0,0 +1,39 @@
+package com.example.demo.db;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.TransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJdbcRepositories
+public class RepositoryTestConfiguration extends AbstractJdbcConfiguration {
+
+    @Bean(destroyMethod = "shutdown")
+    public EmbeddedDatabase embeddedDataSource() {
+
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.HSQL)
+                .generateUniqueName(true)
+                .addScripts("classpath:com/example/demo/db/Create_HSQLDB.sql")
+                .build();
+    }
+
+    @Bean
+    public NamedParameterJdbcOperations jdbcOperations(DataSource dataSource) {
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public TransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
